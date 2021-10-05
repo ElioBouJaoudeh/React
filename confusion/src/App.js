bfcd50839a96bc233b2968297e753a526356b3e4
ElioBouJaoudeh <elionaoum.boujaoudeh@gmail.com>
2021-10-05 20:58:43 +0300

    React Router

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 
 import { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 
 class App extends Component {
 
   render() {
     return (
-      <Main />
+      <BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
+    </BrowserRouter>
     );
   }
 }
